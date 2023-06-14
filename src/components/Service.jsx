d40adbdc501f89859969923c3b014850a8e7176c
hellonewworldjs2023 <hello.new.worldJS.2023@gmail.com>
2023-06-14 15:03:01 +0900

    レスポンシブ作業中

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -5,18 +5,12 @@ const Service = (props) => {
   return (
     <div >
         <h3 className='text-xl mb-2'>設備</h3>
-        <table className='w-1/2 mx-auto border-2 border-black '>
-            <tbody>
-                <tr>
-                    <td className='border border-black text-left'>24時間営業</td><td className='border border-black'>{allTime ? "〇":"-"}</td><td className='border border-black text-left'>館内休憩スペース</td><td className='border border-black'>{restArea ? "〇":"-"}</td>
-                    <td className='border border-black text-left'>食事処</td><td className='border border-black'>{eatingSpace ? "〇":"-"}</td>
-                </tr>
-                <tr>
-                    <td className='border border-black text-left'>漫画</td><td className='border border-black'>{comics ? "〇":"-"}</td><td className='border border-black text-left'>給水器</td><td className='border border-black'>{waterDispenser ? "〇":"-"}</td>
-                    <td className='border border-black text-left'>駐車場</td><td className='border border-black'>{parkingArea ? "〇":"-"}</td>
-                </tr>
-            </tbody>
-        </table>
+        <ul className='w-1/3 mx-auto border-2 border-black '>
+                <li className='border border-black text-left inline '>24時間営業</li><li className='border border-black inline w-4'>{allTime ? "〇":"-"}</li><li className='border border-black text-left inline'>館内休憩スペース</li><li className='border border-black inline lg:block'>{restArea ? "〇":"-"}</li>
+                <li className='border border-black text-left inline'>食事処</li><li className='border border-black '>{eatingSpace ? "〇":"-"}</li>
+                <li className='border border-black text-left inline'>漫画</li><li className='border border-black'>{comics ? "〇":"-"}</li><li className='border border-black text-left inline'>給水器</li><li className='border border-black inline'>{waterDispenser ? "〇":"-"}</li>
+                <li className='border border-black text-left inline'>駐車場</li><li className='border border-black'>{parkingArea ? "〇":"-"}</li> 
+        </ul>
     </div>
   )
 }
